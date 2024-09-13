aa45bd0cc25a0e5373c3f6bba71e8f4e260f8474
Ahmed Abdullai <ahmedabdullai@outlook.com>
2024-09-14 01:15:57 +0200

    add extension method project

---
+++ MoshExtensionMethods/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    internal static class StringExtensions
+    {
+        public static string Shorten(this string s, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = s.Split(' ');
+
+            if (words.Length < numberOfWords)
+                return s;
+
+            return string.Join(" ", words.Take(numberOfWords));
+        }
+    }
+}
