a69e0b30881649ff8324bc2a92b46d86a7ce6b35
Ahmed Abdullai <ahmedabdullai@outlook.com>
2024-09-14 12:54:41 +0200

    add dynamic project

---
+++ MoshDynamic/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+
+//object obj = "Ahmed";
+
+////obj.GetHashCode();
+
+//var methodInfo = obj.GetType().GetMethod("GetHashCode");
+//methodInfo.Invoke(null, null);
+
+//dynamic excelObject = "Ahmed";
+//excelObject.Optimize();
+
+
+
+
+
+//dynamic name = "Ahmed";
+////name++   -> dont work in this step
+//name = 10;
+//name++; // -> here works
+//Console.WriteLine(name);
+
+//var a = "";
+
+
+
+
+
+//dynamic a = 10;
+//dynamic b = 5;
+
+//var c =  a + b;
+
+
+
+
+
+
+int i = 5;
+dynamic d = i;
+
+long l = d;
