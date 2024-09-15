4ec15e014bd2f3e5044e76b6a4aa602b629b7412
Ahmed Abdullai <ahmedabdullai@outlook.com>
2024-09-16 00:46:22 +0200

    add lambda expressions

---
+++ MoshLambdaExpressions/Program.cs
@@ -0,0 +1,46 @@
+﻿//static int Square(int number)
+//{
+//    return number * number;
+//}
+
+
+////args => expression
+
+//// () => ...
+//// x => ...
+//// (x,y,z) => ...
+
+
+
+//Func<int, int> square = number => number * number;
+
+//Console.WriteLine(square(5));
+
+
+
+
+
+
+//const int factor = 5;
+
+//Func<int, int> multiplier = n => factor * n;
+
+//var result = multiplier(10);
+
+//Console.WriteLine(result);
+
+
+
+
+
+
+using MoshLambdaExpressions;
+
+var books = new BookRepository().GetBooks();
+
+var cheapBooks = books.FindAll(b => b.Price < 10);
+
+foreach (var book in cheapBooks)
+{
+    Console.WriteLine(book.Title);
+}
