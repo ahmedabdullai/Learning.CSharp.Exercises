6b3ddfffdfeff121b97fc2f94bfde90be3146283
Ahmed Abdullai <ahmedabdullai@outlook.com>
2024-09-16 02:22:08 +0200

    add delegates

---
+++ MoshDelegates/PhotoFilters.cs
@@ -0,0 +1,24 @@
+﻿namespace MoshDelegates
+{
+    internal class PhotoFilters
+    {
+        public PhotoFilters()
+        {
+        }
+
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply Brightness");
+        }
+
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply Contrast");
+        }
+
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize Photos");
+        }
+    }
+}
---
+++ MoshDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+using MoshDelegates;
+
+var filters = new PhotoFilters();
+
+//PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+Action<Photo> filterHandler = filters.ApplyBrightness;
+filterHandler += filters.ApplyContrast;
+filterHandler += RemoveRedEyeFilter;
+
+new PhotoProcessor().Process("photo.jpg", filterHandler);
+
+
+
+static void RemoveRedEyeFilter(Photo photo)
+{
+    Console.WriteLine("Red eye removed");
+}
